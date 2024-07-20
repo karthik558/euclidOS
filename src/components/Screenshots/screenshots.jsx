57b90b0d26702eb7709237bd022342333425ccbd
KARTHIK LAL <karthik.lal558@gmail.com>
2024-07-20 22:03:19 +0530

    git/website: update the screenshots as per c-rom and switch to json for easy-switching of pictures
    - update the user role from "Source Developer" to "Developer"
    - Introduced new json system for screenshots
    
    Signed-off-by: KARTHIK LAL <karthik.lal558@gmail.com>

--- src/components/Screenshots/screenshots.jsx
+++ src/components/Screenshots/screenshots.jsx
@@ -1,19 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore, { Pagination, Parallax } from "swiper";
+import SwiperCore, { Pagination } from "swiper";
 
 import "swiper/css";
 import "swiper/css/pagination";
-SwiperCore.use([Pagination, Parallax]);
+import screenshots from "../../data/screenshots.json"; // Importing the screenshots JSON
+
+SwiperCore.use([Pagination]);
 
 const Screenshots = () => {
-  const [load, setLoad] = useState(false);
-  useEffect(() => {
-    setLoad(true);
-  }, [load]);
   const paginationRef = React.useRef(null);
+
   return (
-    <section id="screenshots" className="secreen-shots section-padding ">
+    <section id="screenshots" className="secreen-shots section-padding">
       <div className="container">
         <div className="row justify-content-center">
           <div className="col-lg-7 col-md-10">
@@ -29,80 +28,47 @@ const Screenshots = () => {
         <div className="row">
           <div className="col-12">
             <div className="swiper-container">
-              {load ? (
-                <Swiper
-                  speed={1000}
-                  spaceBetween={20}
-                  loop={true}
-                  slidesPerView={5}
-                  pagination={{
-                    clickable: true,
-                    el: paginationRef.current,
-                  }}
-                  className="swiper-wrapper"
-                  breakpoints={{
-                    320: {
-                      slidesPerView: 1,
-                      spaceBetween: 0,
-                    },
-                    640: {
-                      slidesPerView: 2,
-                    },
-                    767: {
-                      slidesPerView: 3,
-                      centeredSlides: false,
-                    },
-                    991: {
-                      slidesPerView: 5,
-                    },
-                  }}
-                >
-                  <SwiperSlide className="swiper-slide">
+              <Swiper
+                speed={1000}
+                spaceBetween={20}
+                loop={true}
+                slidesPerView={5}
+                pagination={{
+                  clickable: true,
+                  el: paginationRef.current,
+                }}
+                className="swiper-wrapper"
+                breakpoints={{
+                  320: {
+                    slidesPerView: 1,
+                    spaceBetween: 0,
+                  },
+                  640: {
+                    slidesPerView: 2,
+                  },
+                  767: {
+                    slidesPerView: 3,
+                    centeredSlides: false,
+                  },
+                  991: {
+                    slidesPerView: 5,
+                  },
+                }}
+              >
+                {screenshots.map((screenshot, index) => (
+                  <SwiperSlide key={index} className="swiper-slide">
                     <div className="item">
                       <div className="screenshot">
-                        <img src="/img/screenshots/1.png" alt="" />
+                        <img
+                          src={screenshot}
+                          alt={`Screenshot ${index + 1}`}
+                          style={{ borderRadius: "15px", width: "100%", height: "auto" }} // Apply border-radius here
+                        />
                       </div>
                     </div>
                   </SwiperSlide>
-                  <SwiperSlide className="swiper-slide">
-                    <div className="item">
-                      <div className="screenshot">
-                        <img src="/img/screenshots/2.png" alt="" />
-                      </div>
-                    </div>
-                  </SwiperSlide>
-                  <SwiperSlide className="swiper-slide">
-                    <div className="item">
-                      <div className="screenshot">
-                        <img src="/img/screenshots/3.png" alt="" />
-                      </div>
-                    </div>
-                  </SwiperSlide>
-                  <SwiperSlide className="swiper-slide">
-                    <div className="item">
-                      <div className="screenshot">
-                        <img src="/img/screenshots/4.png" alt="" />
-                      </div>
-                    </div>
-                  </SwiperSlide>
-                  <SwiperSlide className="swiper-slide">
-                    <div className="item">
-                      <div className="screenshot">
-                        <img src="/img/screenshots/5.png" alt="" />
-                      </div>
-                    </div>
-                  </SwiperSlide>
-                  <SwiperSlide className="swiper-slide">
-                    <div className="item">
-                      <div className="screenshot">
-                        <img src="/img/screenshots/6.png" alt="" />
-                      </div>
-                    </div>
-                  </SwiperSlide>
-                </Swiper>
-              ) : (
-                ""
-              )}
+                ))}
+              </Swiper>
               <div className="swiper-pagination" ref={paginationRef}></div>
             </div>
           </div>
